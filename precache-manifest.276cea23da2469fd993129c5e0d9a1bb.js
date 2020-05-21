1d149ae86e62edb45018ac9f7711620cc742c8c1
Naing Naing Htun <naing2victor@gmail.com>
2020-05-22 01:40:00 +0630

    Updates

---
+++ precache-manifest.276cea23da2469fd993129c5e0d9a1bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331b3447760c8602cd02aed186f74f90",
+    "url": "/gh-pages-react/index.html"
+  },
+  {
+    "revision": "76fba196cbbc238a6857",
+    "url": "/gh-pages-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3556f985271842c7c29",
+    "url": "/gh-pages-react/static/js/2.dd28c8e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-react/static/js/2.dd28c8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76fba196cbbc238a6857",
+    "url": "/gh-pages-react/static/js/main.ee8da342.chunk.js"
+  },
+  {
+    "revision": "aa449ba33b16f4f954e8",
+    "url": "/gh-pages-react/static/js/runtime-main.ab2583cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
